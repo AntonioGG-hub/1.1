0ba0aa561487562decc6a743b19f192aea20df22
Adrian Torres <adctg2799@gmail.com>
2023-05-08 12:57:53 -0500

    Agrega interfaz gráfica y lógica de conversión de monedas

--- src/view/Home.java
+++ src/view/Home.java
@@ -12,6 +12,8 @@ import javax.swing.BoxLayout;
 import java.awt.CardLayout;
 import javax.swing.JToolBar;
 import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
@@ -21,6 +23,16 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -42,6 +54,8 @@ import javax.swing.JSplitPane;
 import javax.swing.JInternalFrame;
 import javax.swing.JDesktopPane;
 import java.awt.Cursor;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Home extends JFrame {
 
@@ -82,12 +96,6 @@ public class Home extends JFrame {
 		bl_contentPane.setVgap(50);
 		contentPane.setLayout(bl_contentPane);
 		
-		JLabel lblTitulo = new JLabel("Bienvenido al conversor de unidades");
-		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTitulo.setFont(new Font("Monospaced", Font.PLAIN, 14));
-		lblTitulo.setBorder(new EmptyBorder(50, 0, 0, 0));
-		contentPane.add(lblTitulo, BorderLayout.NORTH);
-		
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new EmptyBorder(20, 70, 20, 70));
@@ -96,21 +104,105 @@ public class Home extends JFrame {
 		
 		contentPane.add(panel, BorderLayout.CENTER);
 		
+		JComboBox cbConversores = new JComboBox();
+		cbConversores.setMaximumRowCount(3);
+		cbConversores.setModel(new DefaultComboBoxModel(new String[] {"Conversor de monedas", "Conversor de unidades"}));
+		cbConversores.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel.add(cbConversores);
 		
-		JButton btnConvertirMonedas = new JButton("Convertir monedas");
-		btnConvertirMonedas.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		JButton btnContinuar = new JButton("Continuar");
+		btnContinuar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String opcion= (String) cbConversores.getSelectedItem();
+				if(opcion=="Conversor de unidades") {
+					
+				}
+				else if(opcion == "Conversor de monedas"){
+					List<List<String>> lstOpciones= new ArrayList<>();
+					String[] valores = new String[8];
+					int indiceValores=0;
+					lstOpciones.add(new ArrayList<String>(Arrays.asList("COP","EU","Peso colombiano", "Euro", "4947.14")));
+					lstOpciones.add(new ArrayList<String>(Arrays.asList("COP","GBP","Peso colombiano", "Libra esterlina", "5670.51")));
+					lstOpciones.add(new ArrayList<String>(Arrays.asList("COP","JPY","Peso colombiano", "Yen japonés", "33.31")));
+					lstOpciones.add(new ArrayList<String>(Arrays.asList("COP","KRW","Peso colombiano", "Won surcoreano", "3.40")));
+					for (int i = 0; i < lstOpciones.size(); i++) {
+						valores[indiceValores] = "De "+lstOpciones.get(i).get(2)+" a "+lstOpciones.get(i).get(3);
+						indiceValores++;
+						valores[indiceValores] = "De "+lstOpciones.get(i).get(3)+" a "+lstOpciones.get(i).get(2);
+						indiceValores++;
+					}
+					Object opcionMoneda = JOptionPane.showInputDialog(
+							null,
+							"Seleccione una opción de conversión:",
+							"Conversor de monedas",
+							JOptionPane.PLAIN_MESSAGE,
+							null,
+							valores,
+							valores[0]);
+					if(opcionMoneda != null) {
+						Object input = JOptionPane.showInputDialog(
+								null,
+								"Ingrese la cantidad de dinero que deseas convertir: ",
+								"Conversor de monedas",
+								JOptionPane.QUESTION_MESSAGE,
+								null,
+								null,
+								null);
+						if(input != null) {
+							try {
+								String opcionConversion = (String) opcionMoneda, opInicial,opFinal;
+								Pattern pattern = Pattern.compile("^\\d+(\\.\\d+)?$");
+							    
+							    if (!pattern.matcher(input.toString()).matches()) {
+							      throw new NumberFormatException();
+							    }
+								double valor = Double.parseDouble(input.toString()),conversion=0,cambio=0;
+					            opcionConversion = opcionConversion.replace("De ", "");
+					            opcionConversion = opcionConversion.replace(" a ", ";");
+					            String[] subCadenas = opcionConversion.split(";");
+					            opInicial = subCadenas[0];
+					            opFinal = subCadenas[1];
+					            for (List<String> lista : lstOpciones) {
+					            	if ((opInicial.equals(lista.get(2)) && opFinal.equals(lista.get(3))) ||
+					            		(opInicial.equals(lista.get(3)) && opFinal.equals(lista.get(2)))) {
+										cambio = Double.parseDouble(lista.get(4));
+										break;
+									}
+								}
+					            if(opInicial.equals("Peso colombiano")) {
+					            	conversion = valor / cambio;
+					            } else {
+					            	conversion = valor * cambio;
+					            }
+					            
+					            DecimalFormat df = new DecimalFormat("#.##");
+					            
+					            JOptionPane.showMessageDialog(null, "Tienes $ "+df.format(conversion)+" "+opFinal, "Resultado", JOptionPane.INFORMATION_MESSAGE);
+					            int respuesta = JOptionPane.showConfirmDialog(null, "¿Desea continuar usando el programa?", "Confirmación", JOptionPane.YES_NO_CANCEL_OPTION);
+					            
+					            if(respuesta == JOptionPane.YES_OPTION) {
+					                System.out.println("El usuario ha decidido continuar usando el programa.");
+					            } else{
+					            	JOptionPane.showMessageDialog(null, "Programa finalizado", "Información", JOptionPane.INFORMATION_MESSAGE);
+					            	System.exit(0);
+					            }
+							}
+							catch(NumberFormatException ex){
+								JOptionPane.showMessageDialog(null, "El valor ingresado no es un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+							}
+						}     
+					}					
+				}
+			}
+		});
+		panel.add(btnContinuar);
 		
-		btnConvertirMonedas.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnConvertirMonedas.setPreferredSize(new Dimension(20, 20));
-		panel.add(btnConvertirMonedas);
-		
-		
-		JButton btnConvertirLongitudes = new JButton("Convertir longitudes");
-		btnConvertirLongitudes.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		
-		btnConvertirLongitudes.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnConvertirMonedas.setPreferredSize(new Dimension(20, 20));
-		panel.add(btnConvertirLongitudes);
+		JLabel lblTitulo = new JLabel("<html>Bienvenido al conversor de unidades<br>Seleccione una opción de conversión</html>");
+		lblTitulo.setVerticalAlignment(SwingConstants.TOP);
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Monospaced", Font.PLAIN, 14));
+		lblTitulo.setBorder(new EmptyBorder(50, 0, 0, 0));
+		contentPane.add(lblTitulo, BorderLayout.NORTH);
         
         
 		
